1f7f2f73df84704932e87a189b4fee9c8ef23b55
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-04-04 10:48:18 +0300

    create stage helper

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,5 +1,21 @@
+import Display from "./Display";
+import Stage from "./Stage";
+import StartButton from "./StartButton";
+
 function Tetris() {
-    return <div></div>
+    return (
+        <div>
+            <Stage />
+            <div>
+                <div>
+                    <Display text="Score" />
+                    <Display text="Rows" />
+                    <Display text="Level" />
+                </div>
+                <StartButton />
+            </div>
+        </div>
+    );
 }
 
 export default Tetris;
