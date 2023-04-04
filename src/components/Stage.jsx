ba77e389875eee26d2ae9a8c5045fd84f21ff2ea
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-04-04 11:16:24 +0300

    Create types of block and fn that give random block
    This logic incapsulated in tetrominos helper

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -1,5 +1,16 @@
-function Stage() {
-    return <div></div>
+import Cell from "./Cell";
+
+import { nanoid } from "nanoid";
+
+function Stage(props) {
+    const { stage } = props;
+    return <div>
+        {
+            stage.map(row => row.map((cell, idx) => (
+                <Cell key={nanoid() + idx} type={cell[0]} />
+            )))
+        }
+    </div>
 }
 
 export default Stage;
--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -2,10 +2,12 @@ import Display from "./Display";
 import Stage from "./Stage";
 import StartButton from "./StartButton";
 
+import { createStage } from "../helpers/gameHelpers";
+
 function Tetris() {
     return (
         <div>
-            <Stage />
+            <Stage stage={createStage()}/>
             <div>
                 <div>
                     <Display text="Score" />
